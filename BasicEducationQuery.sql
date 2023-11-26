f25782ad5fcc0af1e393ea25a01ee94eada60a6f
iTyrizzle <148303538+iTyrizzle@users.noreply.github.com>
2023-11-26 00:05:38 -0600

    Add files via upload

---
+++ BasicEducationQuery.sql
@@ -0,0 +1,57 @@
+--CREATE TABLE NoFormalEducation
+--(Entity varchar (50),
+--Code varchar (50),
+--Year int,
+--)
+
+--CREATE TABLE FormalEducation
+--(Entity varchar (50),
+--Code varchar (50),
+--Year int,
+--)
+
+--sp_rename 'NoFormalEducation' , 'PopulationNoFormalEducation'
+
+--sp_rename 'FormalEducation' , 'PopulationFormalEducation'
+
+--ALTER TABLE PopulationNoFormalEducation
+--ADD Population int
+
+--ALTER TABLE PopulationFormalEducation
+--ADD Population int
+
+--INSERT INTO PopulationNoFormalEducation VALUES
+--('Australia', 'AUS', 2015, 0),
+--('Australia', 'AUS', 2020, 0),
+--('Brazil', 'BRA', 2015, 8),
+--('Brazil', 'BRA', 2020, 6),
+--('China', 'CHN', 2015, 7),
+--('China', 'CHN', 2020, 5),
+--('France', 'FRA', 2015, 2),
+--('France', 'FRA', 2020, 1),
+--('Japan', 'JPN', 2015, 0),
+--('Japan', 'JPN', 2020, 0),
+--('UnitedStates', 'USA', 2015, 1),
+--('UnitedStates', 'USA', 2020, 0)
+
+--INSERT INTO PopulationFormalEducation VALUES
+--('Australia', 'AUS', 2015, 99),
+--('Australia', 'AUS', 2020, 100),
+--('Brazil', 'BRA', 2015, 92),
+--('Brazil', 'BRA', 2020, 94),
+--('China', 'CHN', 2015, 93),
+--('China', 'CHN', 2020, 95),
+--('France', 'FRA', 2015, 98),
+--('France', 'FRA', 2020, 99),
+--('Japan', 'JPN', 2015, 100),
+--('Japan', 'JPN', 2020, 100),
+--('UnitedStates', 'USA', 2015, 99),
+--('UnitedStates', 'USA', 2020, 100)
+
+--SELECT *
+--FROM PopulationNoFormalEducation
+
+-- 'Population' name change to 'Population_Percentage' has not worked on the MS SQL Server 2016 for some, odd reasoning. 
+
+
+
